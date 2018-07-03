d74fad1e287b9c210eb99d3d8b02893d9023cdee
Markell Owens <markellowens@mac.com>
2018-07-03 20:14:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ function dec(a) {
   return a - 1;
 }
 function makeInt(a) {
-  return parseInt(a); 
+  return parseInt(a, 10); 
 }
 function preserveDecimal(a) {
   return parseFloat(a);
 }
-function preserveDecimal 
+function preserveDecimal(a, sfsfdfdfs){
+  return 
+}
